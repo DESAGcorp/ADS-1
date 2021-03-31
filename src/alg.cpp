ade63ea64e47bf15cddf6574e311fe4159f8e704
DESAGcorp <79166846+DESAGcorp@users.noreply.github.com>
2021-03-31 15:56:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,31 +1,29 @@
-// Copyright 2021 NNTU-CS
-
-int cbinsearch(int *arr, int size, int value) {
+int cbinsearch(int *arr, int size, int value){
 int cnt = 0;
-int left = 0, right = size, mid, BS;
-while (left <= right) {
+int left = 0, right = size - 1, mid, BS = 0;
+while (left <= right){
 mid = left + (right - left) / 2;
-if (value < arr[mid]) {
-right = mid - 1;
-} else if (value > arr[mid]) {
-left = mid + 1;
-} else {
+if (value < arr[mid]){
+right = mid;
+}else if (value > arr[mid]){
+left = mid;
+}else if (value == arr[mid]){
 BS = mid;
+right = left - 1;
 }
 }
-for (int i = BS; i >= 0; i--) {
-if (arr[i] == value) {
+for (int i = BS; i >= 0; i--){
+if (arr[i] == value){
 cnt++;
 }
 }
-for (int i = BS + 1; i < size; i++) {
-if (arr[i] == value) {
+for (int i = BS + 1; i < size; i++){
+if (arr[i] == value){
 cnt++;
 }
 }
-if (BS == -1) {
-return 0;
-} else {
+if (cnt > 0){
 return cnt;
-}
+}esle{
+return 0;
 }
